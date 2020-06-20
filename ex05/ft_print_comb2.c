cdd4039239ecad9b9ac84ae7789735b74975d532
Ruddik Mulaudzi <rmulaudz@c4r6s8.wethinkcode.co.za>
2020-06-20 10:43:10 +0200

    Finished

---
+++ ex05/ft_print_comb2.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmulaudz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/20 09:03:38 by rmulaudz          #+#    #+#             */
+/*   Updated: 2020/06/20 10:41:48 by rmulaudz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+void ft_print_comb2(void)
+{
+	int n1 = 0;
+	int n2 = 1;
+	
+    char num1 = '0';
+	char num2 = '1';
+
+	while(n1 < 99)
+	{
+		while(n2 < 98)
+		{
+			if(n1 < n2)
+			{
+				ft_putchar(num1);
+				ft_putchar(num2);
+				ft_putchar(',');
+				if(num2 == 9)
+				{
+					num2 = num1 + 1;
+				}
+			}
+			n2++;
+			num2++;
+		}
+		n1++;
+		num1++;
+	//	num2 = num1 + 1;
+	}
+}
+
